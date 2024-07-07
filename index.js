254ff3e15bc032c143523b3c300d72bc973b3992
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-07 10:16:57 +0300

    classfunctionlab

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+
+  
+  const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+  ];
+
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  const faredoubler = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  console.log(fareDoubler(10));
+
+  const faretripler = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  console.log(fareTripler(10));
+
+  const selectDifferentDrivers = function(drivers, selectingFunction) {
+    return selectingFunction(drivers);
+  }
+
+  const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia', 'Jane'];
+
+  console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); // Output: ['Sally', 'Bob']
+  console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
+
+  
+
+
+
+
+
+
+
